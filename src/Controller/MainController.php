075e8d737b872b445d993ca05237d0588a018d90
Dmitro Kopylets <dm.kopylets@gmail.com>
2021-12-01 10:12:56 +0200

    first controller & twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function indexAction()
+    {
+       return $this->render('bkarl/index.html.twig',['a' => 458786767]);
+    }
+}
